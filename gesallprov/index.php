abe1cb561585d2be2a6d490a5dcab54703b914ed
Axel Vestberg <axel.vestberg@gmail.com>
2020-05-25 22:07:28 +0200

    fixed str replace php

--- gesallprov/index.php
+++ gesallprov/index.php
@@ -1,7 +1,7 @@
 <?php
 $url = "https://api.edamam.com/search?q=chicken&app_id=c950b701&app_key=3227595a44a82292164fed1f488323f7&from=0&to=3";
 $parts = parse_url($url);
-$other_html = str_replace('---value---', test, '---value---');
+$other_html = str_replace('---value---', $parts, '---value---');
 echo $other_html;
 // $output = [];
 // parse_str($parts['query'], $output);
