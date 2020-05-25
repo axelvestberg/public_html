e495e07b0543770f16b9e1f05ce2a1c17f5e641e
Axel Vestberg <axel.vestberg@gmail.com>
2020-05-25 22:30:37 +0200

    test

--- gesallprov/index.php
+++ gesallprov/index.php
@@ -1,17 +1,12 @@
 <?php
-
 $html = file_get_contents("index.html");
 $html_pieces = explode("<!--===xxx===-->", $html);
 
 echo "$html_pieces[0]";
 
-$replace = str_replace('---value---', 'test', $html_pieces[1]);
-echo $replace
+$other_html = str_replace('---name---', 'nametest', $html_pieces[1]);
+$other_html = str_replace('---value---', 'valuetest', $other_html);
+echo $other_html;
 
 echo "$html_pieces[2]";
-// $output = [];
-// parse_str($parts['query'], $output);
-// echo $output['page']; // 12
-// $url = "https://api.edamam.com/search?q=chicken&app_id=c950b701&app_key=3227595a44a82292164fed1f488323f7&from=0&to=3";
-// $parts = parse_url($url);
 ?>
