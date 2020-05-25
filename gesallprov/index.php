2d91cdb541be7b9f60f87608afb079abc94545ba
Axel Vestberg <axel.vestberg@gmail.com>
2020-05-25 22:58:13 +0200

    test

--- gesallprov/index.php
+++ gesallprov/index.php
@@ -2,14 +2,17 @@
 
 $html = file_get_contents("index.html");
 $html_pieces = explode("<!--===xxx===-->", $html);
+$nametest = "nametest";
+$valuetest = "valuetest";
 
+$url = "https://api.edamam.com/search?q=chicken&app_id=c950b701&app_key=3227595a44a82292164fed1f488323f7&from=0&to=3";
+$parts = parse_url($url);
 echo "$html_pieces[0]";
 
-foreach($_SERVER as $var_name => $var_value) {
-    $other_html = str_replace('---name---', $var_name, $html_pieces[1]);
-    $other_html = str_replace('---value---', $var_value, $other_html);
-    echo $other_html;
-}
+$other_html = str_replace('---name---', $nametest, $html_pieces[1]);
+$other_html = str_replace('---value---', $valuetest, $other_html);
+$other_html = str_replace('---recept---', $parts, $other_html)
+echo "$other_html";
 
 echo "$html_pieces[2]";
 ?>
