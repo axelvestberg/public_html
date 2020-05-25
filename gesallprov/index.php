69beacd6a7dabb9e88a44d1f630d0526e5d8d2ea
Axel Vestberg <axel.vestberg@gmail.com>
2020-05-25 22:41:31 +0200

    test

--- gesallprov/index.php
+++ gesallprov/index.php
@@ -1,13 +1,14 @@
 <?php
+
 $html = file_get_contents("index.html");
 $html_pieces = explode("<!--===xxx===-->", $html);
+
 $nametest = "nametest";
 $valuetest = "valuetest";
 echo "$html_pieces[0]";
 
 $other_html = str_replace('---name---', $nametest, $html_pieces[1]);
-$other_html = str_replace('---value---', $valuetest, $other_html);
-echo $other_html;
+echo "$other_html";
 
 echo "$html_pieces[2]";
 ?>
