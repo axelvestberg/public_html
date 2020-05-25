732771f6965bd3cc938a1522357af86718a74b4d
Axel Vestberg <axel.vestberg@gmail.com>
2020-05-25 23:20:15 +0200

    test

--- gesallprov/index.php
+++ gesallprov/index.php
@@ -6,13 +6,20 @@ $nametest = "nametest";
 $valuetest = "valuetest";
 
 $url = "https://api.edamam.com/search?q=chicken&app_id=c950b701&app_key=3227595a44a82292164fed1f488323f7&from=0&to=3";
-$parts = parse_url($url);
+// $parts = parse_url($url);
+$obj = json_decode($url);
+
+foreach($obj->hits as $key => $value) {
+    $other_html = str_replace('---name---', $key, $html_pieces[1]);
+    $other_html = str_replace('---value---', $value, $other_html);
+    echo $other_html;
+}
 echo "$html_pieces[0]";
 
-$other_html = str_replace('---name---', $nametest, $html_pieces[1]);
-$second = str_replace('---value---', $valuetest, $other_html);
-$third = str_replace('---recept---', $parts, $second);
-echo "$third";
+// $other_html = str_replace('---name---', $nametest, $html_pieces[1]);
+// $second = str_replace('---value---', $valuetest, $other_html);
+// $third = str_replace('---recept---', $parts, $second);
+// echo "$third";
 
 echo "$html_pieces[2]";
 ?>
