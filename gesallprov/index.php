62246acc70940b67bda945e64976e10f97376db5
Axel Vestberg <axel.vestberg@gmail.com>
2020-06-23 16:17:30 +0200

    fixed some new things

--- gesallprov/index.php
+++ gesallprov/index.php
@@ -1,23 +1,16 @@
 <?php
 
-$numberOfRecipes = 20;
-
-if (!empty($_POST['showmore'])) {
-    showMore();
-}
-
-function showMore() {
-    $numberOfRecipes = $numberOfRecipes + 4;
-    print_r($numberOfRecipes);
-}
-// $query = http_build_query($_GET['health'], $_GET['cuisineType']);
 $base_url = "https://api.edamam.com/search?";
 $app_key = "3227595a44a82292164fed1f488323f7";
 $app_id = "c950b701";
 
 $recipe_has_time = "&time=1%2B";
 $from = 0;
-$to = $numberOfRecipes;
+if (empty($_GET['results'])) {
+    $to = 20; 
+} else {
+    $to = $_GET['results'];
+}
 
 if (!empty($_GET['q'])) {
     $search_string = urlencode($_GET['q']);
@@ -47,22 +40,14 @@ $query_array = json_decode($query_json, true);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/870e39642e.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="styles.css"/>
+    <script>
+        function toggle(x) {
+            x.classList.toggle("fas")
+        }
+    </script>
 </head>
 <body>
     <h1>Search recipes</h1>
-    <?php 
-        // print_r($query_url);
-        // echo '<br>';
-        // print_r(http_build_query(parse_str(array($_GET['health']))));
-        // echo "<br>";
-        // print_r($query_parameters['time']);
-        // echo "<br>";
-        // print_r(json_encode($_GET['health']));
-        // echo "<br>";
-        // print_r($_GET);
-        // echo "<br>";
-        // print_r('health='.implode('&health=',$_GET['health']));
-    ?>
     <div id="search-form">
         <form action="" method="get">
             <input type="text" name="q" value="<?php echo (isset($_GET['q'])) ? htmlentities($_GET['q']) : '' ?>"/>
@@ -79,32 +64,39 @@ $query_array = json_decode($query_json, true);
             <label>Peanut free
             <input type="checkbox" name="health[]" value="peanut-free" <?php if (!empty($_GET['health'])) echo in_array('peanut-free', $_GET['health']) ? "checked" : '' ?>/>
             <span class="checkmark"></span>
+            <br>
+            <label>Amount of results to fetch
+            <select name="results" selected="40">
+                <option value="20">20</option>
+                <option value="40">40</option>
+                <option value="60">60</option>
+                <option value="80">80</option>
+            </select>
         </form>
     </div>
 <br/>
     <?php
     echo '<div class="container">';
     echo '<div id="showing">';
-    echo "showing " . $numberOfRecipes . " of " . $query_array['count'] . " recipes";
+    echo "showing " . $to . " of " . $query_array['count'] . " recipes";
     echo '</div>';
     echo '<div class="recipe-container">';
     if (!empty($query_array)) {
         foreach($query_array['hits'] as $recipe) {
+            echo '<div id="test">';
             echo '<a href="' . $recipe['recipe']['url'] . '">';
             echo '<div>';
             echo '<img src="' . $recipe['recipe']['image'] . '" width="150" height="150">';
             echo '<span title="' . $recipe['recipe']['label'] . '" class="label">' . $recipe['recipe']['label'] . '</span>';
             echo '<span class="label">' . $recipe['recipe']['totalTime'] . ' minutes </span>';
-            echo '<i id="like" class="far fa-heart"></i>';
             echo '</div>';
             echo '</a>';
+            echo '<i onclick="toggle(this)" id="like" class="far fa-heart"></i>';
+            echo '</div>';
         }
     }
     echo '</div>';
     echo '</div>';
     ?>
-    <form action="" method="post">
-        <button id="showmore" type="submit" onclick="getMoreRecipes()"></i>Show more recipes</button>
-    </form>
 </body>
 </html>
